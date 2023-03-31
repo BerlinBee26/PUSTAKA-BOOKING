4a99e29c8ecbaa12240080d2bf4d80a134172378
BerlinBee26 <kiduljelek22@gmail.com>
2023-03-31 15:15:05 +0700

    Initial commit

---
+++ application/views/v_index.php
@@ -0,0 +1,10 @@
+<selection>
+    <h1><?php echo $judul ?></h1>
+    <p align='justify'> Hallo saya sedang belajar codeigniter dengan materi </p>
+    <ol type="a">
+        <li>Pengenalan Dasar codeigniter</li>
+        <p align='justify'> Membahas mengenai dasar codeigniter itu sendiri </p>
+        <li>Pengenalan konsep MVC</li>
+        <p align='justify'>Membahas kerangka model MVC</p>
+    <ol>
+<selectin>    
